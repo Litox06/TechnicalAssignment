c7f0857e782663a20973a60c7daf4c2dbbd9eb78
litox06 <litox06@gmail.com>
2022-07-13 23:11:40 -0400

    Creando component input para uso futuro

---
+++ client/src/components/Input/index.js
@@ -0,0 +1,20 @@
+import { process_params } from 'express/lib/router';
+import React from 'react';
+
+// Todo creator (input text for new todo)
+
+export function Input(props) {
+  return (
+    <div className='row' style={{margin: "20px"}}>
+        <div className="input-group">
+            <div className="input-group-prepend">
+                <span className="input-group-text">Todo: </span>
+            </div>
+            
+            <textarea className="form-control"  name={props.name} value={props.value} onChange={props.onChange}></textarea>
+            <button onClick={((e) => {props.onClick(e)})} className='btn btn-primary'>✓</button>
+        </div>
+        
+    </div>
+  );
+};
