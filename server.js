3849a67f06be49a1037253ca053ab01c898bc008
litox06 <litox06@gmail.com>
2022-07-13 22:53:53 -0400

    Agregando configuraciones necesarias para Mongoose

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const path = require("path")
 const mongoose = require("mongoose");
 
 let app = express();
-const PORT = process.env.PORT || 5000 // Server port (same as proxy property in the ./client folder)
+const PORT = process.env.PORT || 5000 // Puerto del servidor (igual a la propiedad proxy en el folder ./client)
+const MONGODB_URI = "mongodb+srv://litox06:todolist@cluster0.c474q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; // URL de conneccion MONGODB Atlas
 
 // Express middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // JSON response
+app.use(express.json()); // Respuesta como JSON
 
-// Requiring files to utilize routes in Express
+// Requiriendo archivo para utilizar rutas en express
 require("./route/apiRoutes")(app);
 
+mongoose.connect(MONGODB_URI);
 app.listen(PORT, () => {
     console.log("Server successfully listening at: http://localhost:" + PORT);
 });
