187603b6ca76b2e1cb615b7e33a7f3a13fd6e2b6
litox06 <litox06@gmail.com>
2022-07-13 23:16:12 -0400

    Agregando componente para renderizar cada to-do

---
+++ client/src/components/Todo/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export function Todo(props) {
+  // Render a todo this functional component needs: onClick - for updating the todo, onClickDelete - for deleting the todo and props.text for the todo text.
+  // props.id so it can be used later in onClickDelete
+  return (
+    <div className='row' style={{margin: "20px"}}>
+        
+        <div className="jumbotron jumbotron-fluid mx-auto" style={{width: "100%", "padding": "0px", borderRadius: "0.25rem" }}>
+        <div style={{width: "50px" }} className="d-flex justify-content-end">
+                <button style={{width: "50px"}} onClick={props.onClick} className='btn btn-primary' id={props.id}>
+                  <i className="fa fa-pencil" style={{fontSize:"36px"}}>
+                  </i>
+                </button>
+            </div>
+          <div className='container'>
+            {props.isComplete === "true" ? <h1 className="display-4 text-center" style={{textDecoration: "line-through"}}>{props.title}</h1> : <h1 className="display-4 text-center">{props.title}</h1>}
+
+            {props.isComplete === "true" ? <p className="lead text-center" style={{textDecoration: "line-through"}}>{props.content}</p> : <p className="lead text-center">{props.content}</p>}
+          
+            
+                  
+            <div style={{width: "50px", margin: "15px"}} className="mx-auto">
+              <button style={{width: "50px" }} onClick={props.onClickDelete} className='btn btn-danger' id={props.id}>x</button>
+            </div>
+   
+            {props.isComplete === "true" ? <p>Created at: {props.date}&#9989;</p> : <p>Created at: {props.date}</p>}
+           </div>
+           
+           
+        </div>
+    
+    </div>
+  );
+};
+
